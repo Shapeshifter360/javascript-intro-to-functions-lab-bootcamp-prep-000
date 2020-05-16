624b5feee22c983841f2bdc181558bcb6c0b2c14
Richard Lucero <lucero0208@yahoo.com>
2020-05-16 23:59:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ function shout(string) {
 return string}
 'Hello!'.toUpperCase() //'HELLO!'
 function shout(string) {return string.toUpperCase()}
+function sayHiToGrandma('I love you!'){
+  return ("I love you too.")
+}
